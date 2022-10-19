ed90371f5173e768aaaa1334e557c0683076014b
Jahmal Blanchard <jablancdesigns@gmail.com>
2022-10-19 18:46:48 -0400

    Add motion to project list

--- components/ProjectsList.tsx
+++ components/ProjectsList.tsx
@@ -9,8 +9,11 @@ import {CgScrollV} from 'react-icons/cg';
 import {TbArrowAutofitWidth} from 'react-icons/tb';
 import Image from 'next/image';
 import { getProjects } from "../src/projectsService";
+import { motion, AnimatePresence, useScroll } from "framer-motion"
 
 const ProjectsList:FC<any> = ({data}): ReactElement => {
+
+    const { scrollYProgress } = useScroll();
 
     const styles = {
         'filterLinks': 'p-4 text-2xl font-bold hover:text-white text-center',
@@ -18,41 +21,50 @@ const ProjectsList:FC<any> = ({data}): ReactElement => {
     }
 
     return (
-        <div className="min-h-screen flex text-primary font-league-spartan w-full pt-[var(--nav-height)] bg-dark">
-            <div className="container mx-auto p-8 relative max-w-7xl">
-                <div className="gap-8 md:flex block">
-                    <div className="md:w-1/3 w-full">
-                        <div className="md:sticky md:top-[calc(var(--nav-height)+2rem)] mb-8 md:mb-0 flex flex-col py-8 md:h-[calc(100vh-(var(--nav-height)*2)-2rem)] h-auto">
-                            
-                            <div className="md:mb-auto text-6xl md:ml-0 pt-8">
-                                <h1 className="text-5xl  mt-auto font-semibold tracking-tight text-white text-center md:text-left drop-shadow-md mb-4 uppercase">
-                                    Projects
-                                </h1>
-                                <p className="text-base text-gray-500 max-w-md md:text-left text-center">
-                                    Lorem, ipsum dolor sit amet consectetur adipisicing elit. Repellat, aut adipisci ea quam dicta voluptatum!
-                                </p>
-                            </div>
-                        </div>
-                    </div>
-                    <div className="flex-1 pr-0 pl-0 md:pl-16 md:pr-4 grid">
-                        <div className='relative grid gap-24'>
-                            {(data.length > 0) && data.map( (project, idx) => {
-                                return <div key={project.id}
-                                    className="border border-transparent hover:drop-shadow-primary cursor-pointer">
-                                    <h2 className={styles.title}>{project.title}</h2>
-                                    <div className="transition-all ease-in-out box-content">
-                                        <Image src='https://placehold.jp/1000x650.jpg' width='1000px' height='650px' alt="Preview project Image" />
-                                    </div> 
-                                    <p className="text-gray-300 text-lg font-thin font-league-spartan mt-4 hidden">
-                                        Lorem ipsum dolor, sit amet consectetur adipisicing elit. Fuga nam alias ullam illum minus in necessitatibus. 
+        <>
+            <motion.div className={`bg-primary fixed top-0 left-0 right-0 origin-top h-[10px] z-10`}  style={{ scaleX: scrollYProgress }}/> 
+            <div className="min-h-screen flex text-primary font-league-spartan w-full pt-[var(--nav-height)] bg-dark">
+                <div className="container mx-auto p-8 relative max-w-7xl">
+                    <div className="gap-8 md:flex block">
+                        <div className="md:w-1/3 w-full">
+                            <div className="md:sticky md:top-[calc(var(--nav-height)+2rem)] mb-8 md:mb-0 flex flex-col py-8 md:h-[calc(100vh-(var(--nav-height)*2)-2rem)] h-auto">
+                                
+                                <div className="md:mb-auto text-6xl md:ml-0 pt-8">
+                                    <h1 className="text-5xl  mt-auto font-semibold tracking-tight text-white text-center md:text-left drop-shadow-md mb-4 uppercase">
+                                        Projects
+                                    </h1>
+                                    <p className="text-base text-gray-500 max-w-md md:text-left text-center">
+                                        Lorem, ipsum dolor sit amet consectetur adipisicing elit. Repellat, aut adipisci ea quam dicta voluptatum!
                                     </p>
                                 </div>
-                            })}
+                            </div>
                         </div>
+                        <motion.div 
+                            className="flex-1 pr-0 pl-0 md:pl-16 md:pr-4 grid">
+                            <motion.div className='relative grid gap-24 overflow-hidden'>
+                                {data.map((project, idx) => (
+                                    <motion.div key={project.id} 
+                                        layoutId={project.id} 
+                                        transition={{ type: "spring", stiffness: 400, damping: 17 }}
+                                        initial={{left: 100}}
+                                        whileInView={{ left: 0 }}
+                                        exit={{ left: 100 }}
+                                        className="border border-transparent hover:drop-shadow-primary cursor-pointer relative">
+                                        <motion.h2 className={styles.title}>{project.title}</motion.h2>
+                                        <motion.div className="transition-all ease-in-out box-content">
+                                            <Image src='https://placehold.jp/1000x650.jpg' width='1000px' height='650px' alt="Preview project Image" />
+                                        </motion.div> 
+                                        <motion.p className="text-gray-300 text-lg font-thin font-league-spartan mt-4 hidden">
+                                            Lorem ipsum dolor, sit amet consectetur adipisicing elit. Fuga nam alias ullam illum minus in necessitatibus. 
+                                        </motion.p>
+                                    </motion.div>
+                                ))}
+                            </motion.div>
+                        </motion.div>
                     </div>
                 </div>
             </div>
-        </div>
+        </>
     )
 }
 
