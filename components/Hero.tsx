09c74d1ffbf7985bc0b0e6ca3733ab04a69c29ea
Jahmal Blanchard <jablancdesigns@gmail.com>
2022-10-21 17:35:26 -0400

    home page animations

--- components/Hero.tsx
+++ components/Hero.tsx
@@ -1,6 +1,7 @@
 import Button from "./Button";
 import {FC, ReactElement} from 'react';
 import Link from 'next/link';
+import { motion } from "framer-motion";
 
 const Hero:FC<any> = (props): ReactElement => {
     const styles = {
@@ -16,17 +17,26 @@ const Hero:FC<any> = (props): ReactElement => {
 
     return (
       <div className={styles.hero}>
-          <div className={styles.headline}>
-            <h1 className="text-dark z-2 relative tracking-tighter md:text-6xl lg:text-7xl text-4xl font-semibold p-4 md:p-8  font-league-spartan">
+          <div 
+            className={styles.headline}>
+            <motion.h1 
+              className="text-dark z-2 relative tracking-tighter md:text-6xl lg:text-7xl text-4xl font-semibold p-4 md:p-8  font-league-spartan"
+              initial={{scale: 0, opacity: 0}}
+              whileInView={{scale: [0, 1.2, 0.9,  1], opacity: [0,1]}}
+              transition={{type: "spring", stiffness: 400, damping: 17 }}>
               Hi I&apos;m Jahmal, a Frontend Developer & Designer based in Alpharetta, GA.
-            </h1>
-            <div className="flex items-center gap-8 justify-center md:flex-row flex-col">
+            </motion.h1>
+            <motion.div 
+              className="flex items-center gap-8 justify-center md:flex-row flex-col relative"
+              initial={{top: 50, opacity: 0}}
+              whileInView={{top: [-30, 10, 0], opacity: 1}}
+              transition={{type: "spring", stiffness: 400, damping: 17, delay: .75 }}>
               <Link href="/projects">
                   <a className="app-button"> 
                   The Work
                   </a>
               </Link>
-            </div>
+            </motion.div>
           </div>
           
           <div className="hero__background absolute left-0 top-0 leading-[0.7] text-center w-screen h-screen overflow-hidden flex flex-col  break-all justify-center">
