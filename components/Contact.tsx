ec90eac2d97dfc2ca576426372ea8a69668ee559
Jahmal Blanchard <jablancdesigns@gmail.com>
2023-07-04 15:39:07 -0400

    integrate netlify form submission

--- components/Contact.tsx
+++ components/Contact.tsx
@@ -1,10 +1,12 @@
 import { ReactElement, useState } from "react";
 import Input from "./Input";
 import useContactForm from "../hooks/useContactForm";
+import { useRouter } from "next/router";
 
 const Contact = (): ReactElement => {
     const {formData, handleFormData} = useContactForm({});
     const [submitClicked, setSubmitClicked] = useState(false)
+    const router = useRouter()
 
     const encode = (data) => {
         return Object.keys(data)
@@ -14,13 +16,17 @@ const Contact = (): ReactElement => {
 
     const handleSubmit = (e) => {
         e.preventDefault()
+        const form = e.target
         fetch("/", {
             method: "POST",
             headers: { "Content-Type": "application/x-www-form-urlencoded" },
-            body: encode({ "form-name": "contactMe", ...formData })
+            body: encode({ 
+                "form-name": form.getAttribute('name'), 
+                ...formData 
+            })
           })
             .then(() => {
-                window.location.href = "/success"
+                router.push("/success")
             })
             .catch(error => alert(error));
     }
@@ -29,7 +35,13 @@ const Contact = (): ReactElement => {
         <div className="bg-black flex flex-grow h-screen py-[var(--nav-height)]">
             <div className="m-auto max-w-xl w-full px-4">
                 <h1 className="text-center text-5xl text-white font-bold mb-6">Contact Me</h1>
-                <form className="flex flex-col gap-4" name="contactMe" onSubmit={handleSubmit}>
+                <form className="flex flex-col gap-4" 
+                    name="contactMe" 
+                    onSubmit={handleSubmit}
+                    method="post"
+                    action="/success/"
+                    data-netlify="true"
+                    data-netlify-honeypot="bot-field">
                     <input type="hidden" name="form-name" value="contactMe" />
                     <div className="flex gap-4 w-full">
                         <div className="w-1/2">
